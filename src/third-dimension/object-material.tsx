9802ee693fa7fb0b37323fe24549a9f3f6e42e35
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-15 11:27:52 +0545

    style: Remove unused comments and update ambient light intensity in viewer

--- src/third-dimension/object-material.tsx
+++ src/third-dimension/object-material.tsx
@@ -20,7 +20,7 @@ export function ObjectMaterial() {
 		<meshPhysicalMaterial
 			attach='material'
 			map={texture.color}
-			// displacementMap={texture.displacement}
+			displacementMap={texture.displacement}
 			roughnessMap={texture.roughness}
 		/>
 	)
--- src/third-dimension/viewer.tsx
+++ src/third-dimension/viewer.tsx
@@ -1,12 +1,26 @@
-import { OrbitControls, Sphere } from '@react-three/drei'
+import { Bounds, OrbitControls, Sphere, SpotLight, useDepthBuffer } from '@react-three/drei'
 
 import { ObjectMaterial } from './object-material.tsx'
 import { Studio } from './studio.tsx'
 
 function Lights() {
+	const depthBuffer = useDepthBuffer()
 	return (
 		<>
-			<ambientLight intensity={5} />
+			{/*<ambientLight intensity={Math.PI / 2} />*/}
+			<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+			<pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+			<SpotLight position={[10, 10, 10]} depthBuffer={depthBuffer}>
+				{/*<SpotLightShadow*/}
+				{/*	distance={0.4} // Distance between the shadow caster and light*/}
+				{/*	alphaTest={0.5} // Sets the alpha value to be used when running an alpha test. See Material.alphaTest*/}
+				{/*	scale={1} //  Scale of the shadow caster plane*/}
+				{/*	map={undefined} // Texture - Pattern of the shadow*/}
+				{/*	shader={undefined} // Optional shader to run. Lets you add effects to the shadow map. See bellow*/}
+				{/*	width={512} // Width of the shadow map. The higher the more expnsive*/}
+				{/*	height={512} // Height of the shadow map. The higher the more expnsive*/}
+				{/*/>*/}
+			</SpotLight>
 		</>
 	)
 }
@@ -16,9 +30,11 @@ function Scene() {
 	return (
 		<group>
 			<Lights />
-			<Sphere args={[1, segments, segments]}>
-				<ObjectMaterial />
-			</Sphere>
+			<Bounds fit>
+				<Sphere args={[1, segments, segments]}>
+					<ObjectMaterial />
+				</Sphere>
+			</Bounds>
 		</group>
 	)
 }
@@ -42,7 +58,7 @@ export function Viewer() {
 	return (
 		<Studio>
 			<Scene />
-			<OrbitControls />
+			<OrbitControls autoRotate={true} />
 		</Studio>
 	)
 }
